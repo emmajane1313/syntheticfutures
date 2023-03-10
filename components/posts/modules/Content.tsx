9426581a44f0e3f993286c6d0b5e37cc855bcccf
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2023-03-10 00:19:13 -0500

    update leading font

--- components/posts/modules/Content.tsx
+++ components/posts/modules/Content.tsx
@@ -7,7 +7,7 @@ const Content: FunctionComponent<ContentProps> = ({
   return (
     <div className="relative w-full h-full grid grid-flow-cols auto-cols-auto text-mainText">
       <div
-        className="relative w-1/2 h-fit justify-self-center text-base font-neueL"
+        className="relative w-1/2 h-fit justify-self-center text-base font-neueL leading-8"
         dangerouslySetInnerHTML={{ __html: postData?.content as string }}
       ></div>
     </div>
