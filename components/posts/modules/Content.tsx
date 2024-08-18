924fa39fb0d7ce26a6baed7dfde69cb6f66d04f9
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2024-08-18 16:37:09 -0400

    roto

--- components/posts/modules/Content.tsx
+++ components/posts/modules/Content.tsx
@@ -7,7 +7,8 @@ const Content: FunctionComponent<ContentProps> = ({
   return (
     <div className="relative w-full h-full grid grid-flow-cols auto-cols-auto text-mainText">
       <div
-        className="relative w-5/6 lg:w-1/2 h-fit justify-self-center text-xl font-neueL leading-8 break-all md:break-words"
+        className="relative w-5/6 lg:w-1/2 h-fit justify-self-center text-xl font-neueL leading-8"
+        id="break"
         dangerouslySetInnerHTML={{ __html: description as string }}
       ></div>
     </div>
