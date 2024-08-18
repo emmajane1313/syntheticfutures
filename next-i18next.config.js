29a362cbc12947064dd8f9fd336ea2f9b7ed89d0
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2024-08-18 16:13:33 -0400

    locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,4 +6,6 @@ module.exports = {
     locales: ["en", "es", "ع", "א", "br", "ук", "د", "あ", "yi", "fr"],
   },
   react: { useSuspense: false },
+  localePath: path.resolve("./public/locales"),
+  reloadOnPrerender: process.env.NODE_ENV === "development",
 };
