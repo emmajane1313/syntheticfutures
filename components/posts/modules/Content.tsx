8c6ea7a5f5f9147825c4615b5084eab7d6606fe2
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2024-08-18 16:18:14 -0400

    locale

--- components/posts/modules/Content.tsx
+++ components/posts/modules/Content.tsx
@@ -7,7 +7,7 @@ const Content: FunctionComponent<ContentProps> = ({
   return (
     <div className="relative w-full h-full grid grid-flow-cols auto-cols-auto text-mainText">
       <div
-        className="relative w-5/6 lg:w-1/2 h-fit justify-self-center text-xl font-neueL leading-8 break-all"
+        className="relative w-5/6 lg:w-1/2 h-fit justify-self-center text-xl font-neueL leading-8 break-words lg:break-all"
         dangerouslySetInnerHTML={{ __html: description as string }}
       ></div>
     </div>
