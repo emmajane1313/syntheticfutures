0914b0071f5eb64d079508a3e3cabfaa67365182
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-21 18:16:13 -0400

    refactor

--- src/app/[lang]/sitemap.ts
+++ src/app/[lang]/sitemap.ts
@@ -1,17 +1,32 @@
 import { MetadataRoute } from "next";
 import posts from "./api/posts.json";
+import { LOCALES } from "../lib/constants";
 
-export default async function sitemap(params: {
-  lang: string;
-}): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     ...posts?.map(({ title }) => ({
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${params?.lang}/post/${title?.[
-        params?.lang  as "en" | "es"
-      ]
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/post/${title?.["en"]
         ?.toLowerCase()
         ?.replaceAll(" ", "")}`,
+      alternates: Object.fromEntries(
+        LOCALES.map((lang) => [
+          lang,
+          `${process.env.NEXT_PUBLIC_BASE_URL}/${lang}/post/${title?.[
+            lang as "en" | "es"
+          ]
+            ?.toLowerCase()
+            ?.replaceAll(" ", "")}`,
+        ])
+      ),
     })),
-    { url: `${process.env.NEXT_PUBLIC_BASE_URL}/${params?.lang}/about` },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+      alternates: Object.fromEntries(
+        LOCALES.map((lang) => [
+          lang,
+          `${process.env.NEXT_PUBLIC_BASE_URL}/${lang}/about`,
+        ])
+      ),
+    },
   ];
 }
