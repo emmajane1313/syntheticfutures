6ed478cd16c72a1d99d80009c747642676e68486
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-21 18:31:34 -0400

    refactor

--- src/app/[lang]/sitemap.ts
+++ src/app/[lang]/sitemap.ts
@@ -6,21 +6,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     ...LOCALES.flatMap((lang) =>
       posts?.flatMap((post) => ({
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}/post/${post?.title?.[
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/${lang}/post/${post?.title?.[
           lang as "en" | "es"
         ]
           ?.toLowerCase()
           ?.replaceAll(" ", "-")}`,
       }))
     ),
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
-      alternates: Object.fromEntries(
-        LOCALES.map((lang) => [
-          lang,
-          `${process.env.NEXT_PUBLIC_BASE_URL}/${lang}/about`,
-        ])
-      ),
-    },
+    ...LOCALES.flatMap((lang) => ({
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${lang}/about`,
+    })),
   ];
 }
