1d54efaef9def13ce121b34deb199331bd88dce0
emmajane1313 <emma.jane@digitalax.xyz>
2025-06-15 18:48:52 -0400

    el mapa

--- src/app/bio/page.tsx
+++ src/app/bio/page.tsx
@@ -1,6 +1,26 @@
 import BioEntry from "@/app/components/Bio/modules/BioEntry";
 import Wrapper from "../components/Common/modules/Wrapper";
 import { getDictionary } from "../[lang]/dictionaries";
+import { Metadata } from "next";
+import { LOCALES_LIVE } from "../lib/constants";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Bio",
+    description: "I'm Emma-Jane. Samples from my timeline.",
+    alternates: {
+      canonical: `https://syntheticfutures.xyz/bio/`,
+      languages: LOCALES_LIVE.reduce((acc, item) => {
+        acc[item] = `https://syntheticfutures.xyz/bio/${item}/`;
+        return acc;
+      }, {} as { [key: string]: string }),
+    },
+    openGraph: {
+      title: "Bio",
+      description: "I'm Emma-Jane. Samples from my timeline.",
+    },
+  };
+};
 
 export default async function Bio() {
   const dict = await (getDictionary as (locale: any) => Promise<any>)("en");
--- src/app/reflections/page.tsx
+++ src/app/reflections/page.tsx
@@ -1,6 +1,26 @@
 import { getDictionary } from "../[lang]/dictionaries";
 import Wrapper from "../components/Common/modules/Wrapper";
 import ReflectionsEntry from "../components/Reflections/modules/ReflectionsEntry";
+import { LOCALES_LIVE } from "../lib/constants";
+import { Metadata } from "next";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Reflections",
+    description: "Visual noise. Confuse those watching machines.",
+    alternates: {
+      canonical: `https://syntheticfutures.xyz/reflections/`,
+      languages: LOCALES_LIVE.reduce((acc, item) => {
+        acc[item] = `https://syntheticfutures.xyz/reflections/${item}/`;
+        return acc;
+      }, {} as { [key: string]: string }),
+    },
+    openGraph: {
+      title: "Reflections",
+      description: "Visual noise. Confuse those watching machines.",
+    },
+  };
+};
 
 export default async function Reflections() {
   const dict = await (getDictionary as (locale: any) => Promise<any>)("en");
